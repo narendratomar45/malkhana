26ca9c43ef4d8b9222c1e40113c7be88d4d92a6a
NARENDRA TOMAR <140541020+narendratomar45@users.noreply.github.com>
2025-03-05 16:10:35 +0530

    Add .env file

--- src/controller/smsController.js
+++ src/controller/smsController.js
@@ -1,35 +1,7 @@
-// const twilio = require("twilio");
-
-// const accountSid = "AC2d24b74eeeee9bf405b9b092c65c621b";
-// const authToken = "2933af6766b98c31e68f4f411954c4b5";
-// const client = new twilio(accountSid, authToken);
-
-// const sendSMS = async (req, res) => {
-//   try {
-//     const { to, message } = req.body;
-
-//     client.messages
-//       .create({
-//         body: message,
-//         to: to,
-//         from: "+14243810662",
-//       })
-//       .then((message) => {
-//         res.status(200).send({ success: true, messageSid: message.sid });
-//       })
-//       .catch((error) => {
-//         res.status(500).send({ success: false, error: error.message });
-//       });
-//   } catch (error) {
-//     res.status(500).send({ success: false, error: error.message });
-//   }
-// };
-// module.exports = sendSMS;
-
 const twilio = require("twilio");
 
-const accountSid = "AC2d24b74eeeee9bf405b9b092c65c621b";
-const authToken = "2933af6766b98c31e68f4f411954c4b5";
+const accountSid = process.env.ACCOUNTSID;
+const authToken = process.env.AUTHTOKEN;
 const client = new twilio(accountSid, authToken);
 
 const sendSMS = async (req, res) => {
@@ -44,8 +16,8 @@ const sendSMS = async (req, res) => {
 
     const sms = await client.messages.create({
       body: message,
-      to: to, 
-      from: "+14243810662", 
+      to: to,
+      from: "+14243810662",
     });
 
     res.status(200).json({ success: true, messageSid: sms.sid });
